8e23f07af07747eadb884bcd82d61b484cb94058
obada othman <obada.oth@gmail.com>
2020-08-09 21:39:55 +0200

    Finished JS Exercises, project still needs some css work

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -1,1 +1,19 @@
 // your code goes in here
+const quotes = [
+  { text: 'text1', author: 'name1' },
+  { text: 'text2', author: 'name2' },
+  { text: 'text3', author: 'name3' },
+  { text: 'text4', author: 'name4' },
+  { text: 'text5', author: 'name5' },
+  { text: 'text6', author: 'name6' },
+];
+const button = document.getElementById('button');
+function showRandomQuote() {
+  const quoteContainer = document.getElementById('quote');
+  const authorContainer = document.getElementById('author');
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  quoteContainer.innerHTML = quote.text;
+  authorContainer.innerHTML = quote.author;
+  //   return;
+}
+button.addEventListener('click', showRandomQuote);
