1a5f493fe1286594bcb37718dc60de7abc95fbd8
obada othman <obada.oth@gmail.com>
2020-08-15 10:12:45 +0200

    Finished project's JS

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -8,3 +8,80 @@
   If the timer finishes the timer should be replaced by the message: Time 's up!
  * 
  */
+// debugger;
+const arrowUp = document.getElementById('up');
+const arrowDown = document.getElementById('down');
+const playBtn = document.getElementById('play');
+const pauseBtn = document.getElementById('pause');
+const stopBtn = document.getElementById('stop');
+const TimeUpMessage = document.getElementById('time-up-message');
+
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+let startCountdown;
+let pauseCountdown = false;
+
+playBtn.addEventListener('click', function () {
+  if (startCountdown === undefined) {
+    startCountdown = setInterval(play, 100);
+  } else {
+    alert('running');
+  }
+});
+function reset() {
+  minutes.innerHTML = 25;
+  seconds.innerHTML = '00';
+  stopCountdown();
+  if ((TimeUpMessage.className = 'show')) {
+    TimeUpMessage.classList.replace('show', 'hide');
+    minutes.classList.replace('hide', 'show');
+    seconds.classList.replace('hide', 'show');
+    startCountdown = undefined;
+    location.reload();
+  }
+}
+stopBtn.addEventListener('click', reset);
+pauseBtn.addEventListener('click', function () {
+  stopCountdown();
+  startCountdown = undefined;
+  pauseCountdown = true;
+});
+arrowUp.addEventListener('click', function () {
+  if (startCountdown === undefined) {
+    minutes.innerHTML++;
+  } else {
+    alert('running');
+  }
+  if (pauseCountdown === true) {
+    alert('running');
+  }
+});
+arrowDown.addEventListener('click', function () {
+  if (startCountdown === undefined) {
+    minutes.innerHTML--;
+  } else {
+    alert('running');
+  }
+  if (pauseCountdown === true) {
+    alert('running');
+  }
+});
+
+function play() {
+  if (seconds.innerHTML != 0) {
+    seconds.innerHTML--;
+  } else if (seconds.innerHTML == 0 && minutes.innerHTML != 0) {
+    seconds.innerHTML = 59;
+    minutes.innerHTML--;
+  }
+  if (seconds.innerHTML == 0 && minutes.innerHTML == 0) {
+    minutes.classList.add('hide');
+    seconds.classList.add('hide');
+    TimeUpMessage.classList.replace('hide', 'show');
+    document.querySelector('body').addEventListener('click', reset);
+  }
+}
+function stopCountdown() {
+  clearInterval(startCountdown);
+}
