50ac46b011035d71c58e1c1c11a2e94e4f648419
obada othman <obada.oth@gmail.com>
2020-08-20 10:28:50 +0200

    added basic html structure to the tip calculator project

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,18 @@
 // Your code goes in here
+// debugger;
+document.querySelector('#header').innerText = 'Tip Calculator';
+function calculateTip() {
+  const billAmount = document.getElementById('bill-amount');
+  const serviceRating = document.getElementById('service-rating');
+  const peopleCount = document.getElementById('people-count');
+  const calculate = document.getElementById('calculate');
+  const output = document.getElementById('tip-amount');
+  calculate.addEventListener('click', calculateTip);
 
-document.querySelector("#app").innerText = "Tip Calculator";
+  billAmount.value === billAmount.innerText;
+
+  return (output.innerText = (billAmount.value * 10) / 100);
+}
+
+// console.log(billAmount.value);
+calculateTip();
